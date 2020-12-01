990fdb06f2b67665764af2f9fc3e3348f39bde66
Alexander Kleshchukevich <keeweery+github@gmail.com>
2020-12-01 23:33:49 +0300

    task7

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from "@material-ui/core/CssBaseline";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import axios from 'axios';
 
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  function(error) {
-    if (error.response.status === 400) {
+  function (error) {
+    const status = error.response.status;
+    if (status === 400) {
       alert(error.response.data?.data);
     }
+
+    if (status === 401 || status === 403) {
+      alert(JSON.stringify(error.response.data));
+    }
+
     return Promise.reject(error.response);
   }
 );
@@ -23,8 +29,8 @@ axios.interceptors.response.use(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
